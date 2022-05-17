812dc91eedfc27961d0c84c80cc29a18edeacdb8
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-05-17 14:24:08 -0400

    feat[logging]: add member join logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Client, Intents } from 'discord.js';
+import { Client, Formatters, Intents, MessageEmbed } from 'discord.js';
 import config from '../config.json';
+import LoggingModule, { LogEventType } from './modules/logging/LoggingModule';
 
 const client = new Client({intents: [
     Intents.FLAGS.GUILDS,
@@ -9,5 +10,24 @@ const client = new Client({intents: [
 ]});
 
 client.once('ready', () => console.log('Ready'));
+
+client.on('guildMemberAdd', async (member) => {
+    const channel = await LoggingModule.fetchLogChannel(LogEventType.JOINS, member.guild);
+
+    const user = member.user;
+
+    const embed = new MessageEmbed()
+        .setAuthor({ name: user.username, iconURL: user.avatarURL() ?? user.defaultAvatarURL })
+        .setTitle('Member Joined')
+        .setDescription(user.username + ' joined the server')
+        .setColor(0x1f8b4c)
+        .addField('Account Created', Formatters.time(user.createdAt, 'R'))
+        .setFooter({ text: 'User ID: ' + user.id })
+        .setTimestamp()
+
+    await channel?.send({
+        embeds: [ embed ]
+    })
+})
 
 client.login(config.token);
