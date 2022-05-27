0286c187506ba7d8203c9f752b7cf12f9f2b138d
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-05-27 00:55:37 -0400

    feat[automod]: add anti-spam for duplicate messages

--- src/handlers/messageCreate.ts
+++ src/handlers/messageCreate.ts
@@ -2,5 +2,5 @@ import { Message } from 'discord.js';
 import AntiSpamModule from '../modules/automod/AntiSpam';
 
 export async function onMessageCreate(message: Message) {
-
+    await AntiSpamModule.process(message);
 }
