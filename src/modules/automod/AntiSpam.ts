d63378160a6722ef74d46babb6f0cde057c011eb
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-05-27 18:23:18 -0400

    feat: add anti-spam immunity for moderators

--- src/modules/automod/AntiSpam.ts
+++ src/modules/automod/AntiSpam.ts
@@ -1,4 +1,4 @@
-import { GuildMember, Message } from 'discord.js';
+import { GuildMember, Message, Permissions } from 'discord.js';
 import { createHash } from 'crypto';
 import { canDelete } from '../../util/checks';
 import LoggingModule from '../logging/LoggingModule';
@@ -79,8 +79,14 @@ export default class AntiSpamModule {
         await UserReputation.modifyReputation(member, -0.3);  // -0.5 total each time they're muted hereafter
     }
 
+    private static shouldIgnore(message: Message): boolean {
+        if(message.guild == null || message.author.bot || message.channel.type == 'DM') return true;
+
+        return message.member?.permissionsIn(message.channel).has(Permissions.FLAGS.MANAGE_MESSAGES) ?? false;
+    }
+
     static async process(message: Message) {
-        if(message.guild == null || message.author.bot || message.member == null) return;
+        if(this.shouldIgnore(message) || message.member == null) return;
 
         const entry = this.setAndGet(message);
         const count = entry.count;
