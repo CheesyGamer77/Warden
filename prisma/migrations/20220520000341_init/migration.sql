6745eb868b6549fe157633278152bef1b9bc6420
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-05-29 12:49:07 -0400

    refactor[db] modify user changes migration
    - Renamed parent folder to be more descriptive
    - No longer drops text filter column

--- prisma/migrations/20220520000341_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `textFilterChannelId` on the `LogConfig` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "LogConfig" DROP COLUMN "textFilterChannelId",
-ADD COLUMN     "userChangesChannelId" TEXT;
