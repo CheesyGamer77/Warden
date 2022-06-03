c1070e29b3f10ccdda29b22e51b617519155ed38
Casey <43181046+CheesyGamer77@users.noreply.github.com>
2022-06-02 22:04:14 -0400

    feat[automod] add support for anti-spam configuration (#15)
    * refactor[db] add automod/antispam config data
    
    Add ability for the anti-spam to be enabled/disabled guild-wide or
    per-channel. Default is disabled guild-wide
    
    * refactor[db] rename user reputation migration
    
    Renamed parent folder to be more descriptive
    
    * refactor[db] rename text filter migration
    
    Renamed parent folder to be more descriptive
    
    * refactor[db] modify user changes migration
    
    - Renamed parent folder to be more descriptive
    - No longer drops text filter column
    
    * refactor[db] modify escalations migration
    
    - Renamed parent folder to be more descriptive
    - No longer adds the text filter column
    
    * refactor: rename anti-spam entry cache symbol
    
    * refactor[automod]: add anti-spam cache for ignored channels
    
    * feat[automod]: anti-spam ignore channels
    
    - Allows for the anti-spam to ignore/unignore individual guild channels
    
    * refactor[automod]: modify anti-spam cache expiry
    
    - Decreases spam entry TTL from 5 minutes to 1 minute
    
    * refactor: temp disable caching
    
    * refactor: add automod config cache
    
    * ci[lint]: stop carring about line break style, trailing commas
    
    * refacor[automod]: remove shouldIgnore in antispam
    
    * feat[automod] antispam ignore guilds
    
    * chore: relint
    
    * fix: reduce query times for automod configs

---
+++ prisma/migrations/20220520000341_user-change-logs/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "LogConfig" ADD COLUMN     "userChangesChannelId" TEXT;
--- prisma/migrations/20220527034022_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "LogConfig" ADD COLUMN     "escalationsChannelId" TEXT,
-ADD COLUMN     "textFilterChannelId" TEXT;
