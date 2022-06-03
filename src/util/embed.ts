7c3b0a5417bbff5c2edfde788109518d3c43da0c
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-06-03 12:40:38 -0400

    refactor[i18n]: localize getEmbedWithTarget util

--- src/util/embed.ts
+++ src/util/embed.ts
@@ -1,8 +1,9 @@
 import { MessageEmbed, User } from 'discord.js';
+import i18next from 'i18next';
 
-export function getEmbedWithTarget(user: User): MessageEmbed {
+export function getEmbedWithTarget(user: User, lng: string): MessageEmbed {
     return new MessageEmbed()
         .setAuthor({ name: user.tag, iconURL: user.avatarURL() ?? user.defaultAvatarURL })
-        .setFooter({ text: 'User ID: ' + user.id })
+        .setFooter({ text: i18next.t('logging.common.footer', { lng: lng, userId: user.id }) })
         .setTimestamp();
 }
