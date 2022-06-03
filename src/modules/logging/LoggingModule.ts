162614c861da1230384b9d4f39826ce75afee2d5
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-06-03 12:46:10 -0400

    refactor[i18n]: localize member leave logs

--- src/modules/logging/LoggingModule.ts
+++ src/modules/logging/LoggingModule.ts
@@ -97,20 +97,27 @@ export default class LoggingModule {
 
     static async logMemberLeft(member: GuildMember | PartialGuildMember) {
         const channel = await this.fetchLogChannel('leaves', member.guild);
+        const lng = member.guild.preferredLocale;
 
         const user = member.user;
 
-        const embed = getEmbedWithTarget(user)
-            .setTitle('Member Left')
-            .setDescription(user.toString() + ' left the server')
+        const embed = getEmbedWithTarget(user, lng)
+            .setTitle(i18next.t('logging.leaves.title', { lng: lng }))
+            .setDescription(i18next.t('logging.leaves.description', {
+                lng: lng,
+                userMention: user.toString()
+            }))
             .setColor('RED');
 
         // a removed member's joined at timestamp has the potential to be null
         let memberSince: string;
         if (member.joinedAt != null) {memberSince = Formatters.time(member.joinedAt, 'R');}
-        else {memberSince = 'Unknown';}
+        else {memberSince = i18next.t('logging.leaves.fields.memberSince.unknown');}
 
-        embed.addField('Member Since', memberSince);
+        embed.addField(
+            i18next.t('logging.leaves.fields.memberSince.name'),
+            memberSince
+        );
 
         await channel?.send({
             content: user.id,
