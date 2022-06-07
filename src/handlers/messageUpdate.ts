2bbd65c9723228f6780d6b49cfec88c63b004cb1
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-06-06 22:09:20 -0400

    chore: remove unneded non-guild check in message updates

--- src/handlers/messageUpdate.ts
+++ src/handlers/messageUpdate.ts
@@ -3,7 +3,7 @@ import LoggingModule from '../modules/logging/LoggingModule';
 
 export async function onMessageUpdate(before: Message | PartialMessage, after: Message | PartialMessage) {
     // only track message content updates and non-partial after messages
-    if (before.content === after.content || after.partial || after.guild == null) { return; }
+    if (before.content === after.content || after.partial) { return; }
 
     await LoggingModule.logMessageEdit(before, after);
 }
