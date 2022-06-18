25c47f8c2ac070b82c0de88d58ab5c332d01c36c
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-06-18 13:08:41 -0400

    feat[util]: add SlashCommand base

---
+++ src/util/commands/slash/SlashCommand.ts
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+
+export abstract class SlashCommandBase {
+    readonly name: string;
+    readonly description: string;
+
+    constructor(name: string, description: string) {
+        this.name = name;
+        this.description = description;
+    }
+
+    abstract process(interaction: CommandInteraction): Promise<void>
+    abstract invoke(interaction: CommandInteraction): Promise<void>
+}
+
+export default abstract class extends SlashCommandBase {
+    readonly dataBuilder: SlashCommandBuilder;
+
+    constructor(name: string, description: string) {
+        super(name, description);
+
+        this.dataBuilder = new SlashCommandBuilder()
+            .setName(name)
+            .setDescription(description);
+    }
+
+    override async process(interaction: CommandInteraction): Promise<void> {
+        if (interaction.commandName === this.name) {
+            const subcommand = interaction.options.getSubcommand(false);
+            const subcommandGroup = interaction.options.getSubcommandGroup(false);
+
+            if (subcommandGroup === null && subcommand == null) {
+                await this.invoke(interaction);
+            }
+        }
+    }
+}
