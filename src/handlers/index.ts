3fa6b214309acd57cf505afd4451343efb34563f
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-06-20 22:25:27 -0400

    fix: add interactionCreate handler export

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -8,11 +8,14 @@ import { onVoiceStateUpdate } from './voiceStateUpdate';
 import { onThreadCreate } from './threadCreate';
 import { onThreadDelete } from './threadDelete';
 import { onThreadUpdate } from './threadUpdate';
+import onInteractionCreate from './interactionCreate';
+
 
 export {
     onGuildMemberAdd,
     onGuildMemberUpdate,
     onGuildMemberRemove,
+    onInteractionCreate,
     onMessageCreate,
     onMessageUpdate,
     onMessageDelete,
