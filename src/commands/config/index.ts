89c49c9b0e3e60f714e0bcfe2f07f48f69d00b02
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-06-22 18:49:37 -0400

    refactor[commands]: change permissions for `/config`
    Changed required permissions to include `MANAGE_GUILD`

--- src/commands/config/index.ts
+++ src/commands/config/index.ts
@@ -1,9 +1,15 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, Permissions } from 'discord.js';
 import { SlashCommand } from '../../util/commands/slash';
 
+/**
+ * `/config` command - Allows for guild configuration customization.
+ *
+ * This requires `MANAGE_GUILD` permissions in order to be used.
+ */
 export default class ConfigCommand extends SlashCommand {
     constructor() {
         super('config', 'Configuration commands');
+        this.dataBuilder.setDefaultMemberPermissions(Permissions.FLAGS.MANAGE_GUILD);
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
