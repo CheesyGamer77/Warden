0197d13f60181ffede9099104cf5efb85ceb1dc0
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-07-13 14:27:42 -0400

    feat[commands]: add `/config name-sanitizer` group
    Added `/config name-sanitizer enable` and `/config name-sanitizer disable`.
    
    These commands enable/disable the name sanitizer respectively.

---
+++ src/commands/config/name-sanitizer/disable.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import i18next from 'i18next';
+import NameSanitizerModule from '../../../modules/automod/NameSanitizer';
+import { Subcommand } from '../../../util/commands/slash';
+
+export default class DisableCommand extends Subcommand {
+    // TODO: Localize command data
+    constructor() {
+        super('disable', 'Disables the Name Sanitizer');
+    }
+
+    override async invoke(interaction: CommandInteraction) {
+        if (interaction.guild == null) return;
+
+        const guild = interaction.guild;
+        const lng = guild.preferredLocale;
+
+        await NameSanitizerModule.setEnabled(guild, true);
+        await interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setDescription(i18next.t('commands.config.name-sanitizer.disabled', {
+                        lng: lng,
+                        emoji: ':white_checkmark:'
+                    }))
+                    .setColor('GOLD')
+            ]
+        });
+    }
+}
