CheesyGamer77/Warden

commit 5f2a2d0e25df40742a02542cf1a47d820f7aaae5

fix: misc logs (#34)
* fix[logging]: incorrect voice move logs

Fixes an issue where voice change logs would state that the before channel and after channel are the same.

See Issue: #32

* [ci skip] fix[logging, locales]: capitalization

Fixes an issue where the `Message` field for message delete logs was not properly capitalized.

See Issue: #33

src/handlers/voiceStateUpdate.ts
@@ -49,6 +49,9 @@ export default async function onVoiceStateUpdate(before: VoiceState, after: Voic
         const from = before.channel;
         const to = after.channel;
 
+        // ensure `from` and `to` aren't the same channel (#32)
+        if (from != null && from.id == after.id) return;
+
         embed = getEmbedWithTarget(member.user, lng)
             .setTitle(i18next.t('logging.voiceEvents.moves.title', { lng: lng }))
             .setDescription(i18next.t('logging.voiceEvents.moves.description', {

commit e147c62bfbc61acedd7139ba9a55ab9720b51c86

[ci skip] fix[logging]: voice change logs
This once again closes #32 by correcting a typo involving an inproper voice channel check, introduced in #34's initial solution.

src/handlers/voiceStateUpdate.ts
@@ -50,7 +50,7 @@ export default async function onVoiceStateUpdate(before: VoiceState, after: Voic
         const to = after.channel;
 
         // ensure `from` and `to` aren't the same channel (#32)
-        if (from != null && from.id == after.id) return;
+        if (from != null && to != null && from.id == to.id) return;
 
         embed = getEmbedWithTarget(member.user, lng)
             .setTitle(i18next.t('logging.voiceEvents.moves.title', { lng: lng }))