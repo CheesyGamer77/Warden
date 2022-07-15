CheesyGamer77/Warden

commit b900fda873ce8e6a1834deade0cc0729ce6683f2

refactor[logging]: retrieving log configurations
Optimized `LoggingModule.retrieveConfiguration(Guild)`'s fallback query.

src/modules/logging/LoggingModule.ts
@@ -20,35 +20,19 @@ interface LogMemberTimeoutOptions {
 export default class LoggingModule {
     private static configCache: ExpiryMap<string, LogConfig> = new ExpiryMap(15 * 1000 * 60);
 
-    private static async createBlankLogConfiguration(guild: Guild): Promise<LogConfig> {
-        const data = await prisma.logConfig.create({
-            data: {
-                guildId: guild.id,
-            },
-        });
-
-        this.configCache.set(guild.id, data);
-
-        return data;
-    }
-
     /**
-     * Fetches a guild's logging configuration.
-     * If the configuration is cached, this will return the cached entry and not query the database.
-     *
-     * This will create a new blank configuration and return the blank entry in the event that the config is not found.
+     * Retrieves a guild's logging configuration.
+     * This returns the cached config entry if found, or query the database for an existing/default configuration otherwise.
      * @param guild The guild to fetch the config of
      * @returns The configuration
      */
     static async retrieveConfiguration(guild: Guild): Promise<LogConfig> {
-        const data = this.configCache.get(guild.id) ?? await prisma.logConfig.findUnique({
-            where: {
-                guildId: guild.id,
-            },
+        const data = { guildId: guild.id };
+        return this.configCache.get(guild.id) ?? await prisma.logConfig.upsert({
+            where: data,
+            update: {},
+            create: data
         });
-
-        // fallback to new blank config if not found in cache or db
-        return data != null ? data : await this.createBlankLogConfiguration(guild);
     }
 
     /**
@@ -176,7 +160,7 @@ export default class LoggingModule {
             }))
             .setColor('BLUE');
 
-        // splitting code below taken from https://stackoverflow.com/a/58204391
+        // splitting code below modified from https://stackoverflow.com/a/58204391
         const parts = message.content.match(/\b[\w\s]{2000,}?(?=\s)|.+$/g) ?? [ message.content ];
         for (const part of parts) {
             embed.addField(

commit 6f36bbc671f3d59f8aed6c849ff9a9f7a23b5c09

fix[logging]: caching log configurations
Fix a bug introduced in b900fda where querying the database for log
configurations did not cache the result.

src/modules/logging/LoggingModule.ts
@@ -4,6 +4,7 @@ import { canMessage } from '../../util/checks';
 import { getEmbedWithTarget } from '../../util/embed';
 import ExpiryMap from 'expiry-map';
 import i18next from 'i18next';
+import Duration from '../../util/duration';
 
 const prisma = new PrismaClient();
 
@@ -18,7 +19,20 @@ interface LogMemberTimeoutOptions {
 }
 
 export default class LoggingModule {
-    private static configCache: ExpiryMap<string, LogConfig> = new ExpiryMap(15 * 1000 * 60);
+    private static configCache: ExpiryMap<string, LogConfig> = new ExpiryMap(Duration.ofMinutes(15).toMilliseconds());
+
+    private static async fetchAndCacheConfiguration(guild: Guild) {
+        const data = { guildId: guild.id };
+        const config = await prisma.logConfig.upsert({
+            where: data,
+            update: {},
+            create: data
+        });
+
+        this.configCache.set(guild.id, config);
+
+        return config
+    }
 
     /**
      * Retrieves a guild's logging configuration.
@@ -26,13 +40,8 @@ export default class LoggingModule {
      * @param guild The guild to fetch the config of
      * @returns The configuration
      */
-    static async retrieveConfiguration(guild: Guild): Promise<LogConfig> {
-        const data = { guildId: guild.id };
-        return this.configCache.get(guild.id) ?? await prisma.logConfig.upsert({
-            where: data,
-            update: {},
-            create: data
-        });
+    static async retrieveConfiguration(guild: Guild) {
+        return this.configCache.get(guild.id) ?? await this.fetchAndCacheConfiguration(guild);
     }
 
     /**
@@ -42,7 +51,7 @@ export default class LoggingModule {
      * @param guild The guild to fetch the log channel from
      * @returns The TextChannel if it exists, else null
      */
-    static async retrieveLogChannel(event: LogEventType, guild: Guild): Promise<TextChannel | null> {
+    static async retrieveLogChannel(event: LogEventType, guild: Guild) {
         const config = await this.retrieveConfiguration(guild);
 
         const channelId = config[event + 'ChannelId' as keyof LogConfig];
@@ -129,8 +138,12 @@ export default class LoggingModule {
 
         // a removed member's joined at timestamp has the potential to be null
         let memberSince: string;
-        if (member.joinedAt != null) {memberSince = Formatters.time(member.joinedAt, 'R');}
-        else {memberSince = i18next.t('logging.leaves.fields.memberSince.unknown');}
+        if (member.joinedAt != null) {
+            memberSince = Formatters.time(member.joinedAt, 'R');
+        }
+        else {
+            memberSince = i18next.t('logging.leaves.fields.memberSince.unknown');
+        }
 
         embed.addField(
             i18next.t('logging.leaves.fields.memberSince.name'),