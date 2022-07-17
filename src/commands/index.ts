f0a4446dc9401a57b007fe79c2d3683e92fb1963
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-07-16 20:37:36 -0400

    refactor(internal): rework
    Move command updates outside of main index file.
    
    Remove command caching due to buggy behavior.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,17 @@
 import CommandListener from '../util/commands/CommandListener';
 import ConfigCommand from './config';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
 
 const listener = new CommandListener(
     new ConfigCommand()
 );
 
-export { listener };
+export async function updateCommands(token: string, clientId: string) {
+    const rest = new REST({ version: '10' }).setToken(token);
+    await rest.put(Routes.applicationCommands(clientId), { body: listener.getCommands() });
+}
+
+export {
+    listener
+};
