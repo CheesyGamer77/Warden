59b0745610945728238f9bca47afc3d194c20dce
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-07-16 20:49:00 -0400

    chore: add log message when updating commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,14 +2,19 @@ import CommandListener from '../util/commands/CommandListener';
 import ConfigCommand from './config';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
+import Warden from '../warden';
 
 const listener = new CommandListener(
     new ConfigCommand()
 );
 
 export async function updateCommands(token: string, clientId: string) {
+    const commands = listener.getCommands();
+
     const rest = new REST({ version: '10' }).setToken(token);
-    await rest.put(Routes.applicationCommands(clientId), { body: listener.getCommands() });
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
+
+    Warden.logger.debug(`Updated ${commands.length} commands`);
 }
 
 export {
