e7f499721bdf4a20e29e46e0cb2721edf8bdc092
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-07-24 17:14:37 -0400

    refactor: rename `AntiSpamModule`
    Rename `AntiSpamModule` to `AntiSpam`

--- src/handlers/messageCreate.ts
+++ src/handlers/messageCreate.ts
@@ -1,6 +1,6 @@
 import { Message } from 'discord.js';
-import AntiSpamModule from '../modules/automod/runners/antispam';
+import AntiSpam from '../modules/automod/runners/antispam';
 
 export default async function onMessageCreate(message: Message) {
-    await AntiSpamModule.process(message);
+    await AntiSpam.process(message);
 }
