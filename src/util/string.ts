801d8d3c49a50f7737dfba05f31dede711b4d511
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-07-28 00:17:27 -0400

    chore: relint

--- src/util/string.ts
+++ src/util/string.ts
@@ -1,8 +1,8 @@
 export function getPastTenseSuffix(input: string) {
-    if(input.length == 0) return input;
+    if (input.length == 0) return input;
 
     const last = input[input.length - 1];
-    if(last.toLowerCase() == 'e') return input.concat('d');
+    if (last.toLowerCase() == 'e') return input.concat('d');
 
     return input.concat('ed');
 }
@@ -10,12 +10,12 @@ export function getPastTenseSuffix(input: string) {
 export function capitalizeFirstLetter(input: string) {
     input = input.toLowerCase();
 
-    switch(input.length) {
-        case 0:
-            return input;
-        case 1:
-            return input.charAt(0).toUpperCase()
-        default:
-            return input.charAt(0).toUpperCase().concat(input.substring(1))
+    switch (input.length) {
+    case 0:
+        return input;
+    case 1:
+        return input.charAt(0).toUpperCase();
+    default:
+        return input.charAt(0).toUpperCase().concat(input.substring(1));
     }
 }
