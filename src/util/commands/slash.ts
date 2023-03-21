9abc079c6f6ed4d46b444e8d73073c8dcd3ae000
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2023-03-21 17:37:36 -0400

    chore: remove redundant slash abstraction
    Remove leftover and redundant slash command abstraction

--- src/util/commands/slash.ts
+++ src/util/commands/slash.ts
@@ -1,12 +1,5 @@
-import { SlashCommand } from 'cheesyutils.js';
+import { GuildSlashCommand } from 'cheesyutils.js';
 import { ChatInputCommandInteraction } from 'discord.js';
-
-export abstract class GuildSlashCommand extends SlashCommand {
-    constructor(name: string, description: string) {
-        super(name, description);
-        this.data.setDMPermission(false);
-    }
-}
 
 export abstract class PermissionLockedSlashCommand extends GuildSlashCommand {
     constructor(name: string, description: string, permissions: string | number | bigint | null | undefined) {
@@ -14,6 +7,7 @@ export abstract class PermissionLockedSlashCommand extends GuildSlashCommand {
         this.data.setDefaultMemberPermissions(permissions);
     }
 
+    // TODO: This should be redundant
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     protected botHasPermissions(_: ChatInputCommandInteraction) {
         return true;
